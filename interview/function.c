51c0b743fd0c6ed079e41f7b403a8e2ee583cf26
张顺通 <shuntongzhang@gmail.com>
2012-05-14 10:27:00 +0800

    add z_strncpy

--- interview/function.c
+++ interview/function.c
@@ -16,6 +16,20 @@ static char *z_strcpy(char *dst, const char *src)
 
 	return dst;
 }
+static char *z_strncpy(char *dst, const char *src, size_t n)
+{
+	size_t i = 0;
+	assert(dst != NULL && src != NULL);
+	
+	for (i = 0; i < n && src[i] != '\0'; i++) {
+		dst[i] = src[i];
+	}
+	for ( ; i < n; i++) {
+		dst[i] = '\0';
+	}
+	
+	return dst;
+}
 
 /**
  * memcpy
